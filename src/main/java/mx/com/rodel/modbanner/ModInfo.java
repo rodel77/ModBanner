9f0b8bf761ab21e02befb17aacc2822c1d778d55
rodel77 <rodel1407@hotmail.com>
2018-03-16 23:10:25 -0600

    Updated to API 7.1.0, (Added two numbers because i uploaded 1.1.4 release without commit)

--- src/main/java/mx/com/rodel/modbanner/ModInfo.java
+++ src/main/java/mx/com/rodel/modbanner/ModInfo.java
@@ -3,5 +3,5 @@ package mx.com.rodel.modbanner;
 public class ModInfo {
 	public final static String NAME = "ModBanner";
 	public final static String ID = "modbanner";
-	public final static String VERSION = "1.1.3";
+	public final static String VERSION = "1.1.5";
 }
